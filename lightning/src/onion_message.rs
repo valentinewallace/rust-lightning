fe5e2599c3adebe0b81add88ae3e8bbad7438983
Valentine Wallace <vwallace@protonmail.com>
2022-05-27 17:47:15 -0700

    Add onion_messages::Packet and adapt construct_onion_packet_with_init_noise for it
    We need to add a new Packet struct because onion message packet hop_data fields
    can be of variable length, whereas regular payment packets are always 1366
    bytes.

--- lightning/src/onion_message.rs
+++ lightning/src/onion_message.rs
@@ -18,11 +18,57 @@ use chain::keysinterface::{KeysInterface, Sign};
 use ln::msgs::DecodeError;
 use ln::onion_utils;
 use util::chacha20poly1305rfc::ChaChaPolyWriteAdapter;
-use util::ser::{IgnoringLengthReadable, LengthReadable, Readable, VecWriter, Writeable, Writer};
+use util::ser::{IgnoringLengthReadable, LengthRead, LengthReadable, Readable, VecWriter, Writeable, Writer};
 
 use core::ops::Deref;
 use io::{self, Read};
 use prelude::*;
+
+#[derive(Clone, Debug, PartialEq)]
+pub(crate) struct Packet {
+	pub(crate) version: u8,
+	pub(crate) public_key: PublicKey,
+	// Unlike the onion packets used for payments, onion message packets can have payloads greater than 1300 bytes.
+	pub(crate) hop_data: Vec<u8>,
+	pub(crate) hmac: [u8; 32],
+}
+
+impl Writeable for Packet {
+	fn write<W: Writer>(&self, w: &mut W) -> Result<(), io::Error> {
+		self.version.write(w)?;
+		self.public_key.write(w)?;
+		w.write_all(&self.hop_data)?;
+		self.hmac.write(w)?;
+		Ok(())
+	}
+}
+
+impl LengthReadable for Packet {
+	fn read<R: LengthRead>(r: &mut R) -> Result<Self, DecodeError> {
+		if r.total_bytes() < 66 {
+			return Err(DecodeError::InvalidValue)
+		}
+		let version = Readable::read(r)?;
+		let public_key = {
+			let mut buf = [0u8;33];
+			r.read_exact(&mut buf)?;
+			PublicKey::from_slice(&buf).map_err(|_| DecodeError::InvalidValue)?
+		};
+		// 1 (version) + 33 (pubkey) + 32 (HMAC) = 66
+		let mut hop_data = vec![0; r.total_bytes() as usize - 66];
+		for i in 0..r.total_bytes() - 66 {
+			let byte: u8 = Readable::read(r)?;
+			hop_data[i as usize] = byte;
+		}
+		let hmac = Readable::read(r)?;
+		Ok(Packet {
+			version,
+			public_key,
+			hop_data,
+			hmac,
+		})
+	}
+}
 
 /// Onion messages have "control" TLVs and "data" TLVs. Control TLVs are used to control the
 /// direction and routing of an onion message from hop to hop, whereas data TLVs contain the onion
