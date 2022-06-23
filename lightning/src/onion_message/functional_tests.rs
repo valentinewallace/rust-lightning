add6cf4666d73f8c8fa994e4269a3bac6140bc16
Valentine Wallace <vwallace@protonmail.com>
2022-06-23 15:51:43 -0400

    Error when attempting to send an OM to a blinded route with 0 hops

--- lightning/src/onion_message/functional_tests.rs
+++ lightning/src/onion_message/functional_tests.rs
@@ -119,3 +119,17 @@ fn too_big_packet_error() {
 	let err = nodes[0].messenger.send_onion_message(hops, Destination::Node(hop_node_id)).unwrap_err();
 	assert_eq!(err, SendError::TooBigPacket);
 }
+
+#[test]
+fn invalid_blinded_route_error() {
+	// Make sure we error as expected if a provided blinded route has 0 hops.
+	let mut nodes = create_nodes(3);
+	let (node1, node2, node3) = (nodes.remove(0), nodes.remove(0), nodes.remove(0));
+
+	let secp_ctx = Secp256k1::new();
+	let mut blinded_route = BlindedRoute::new::<EnforcingSigner, _, _>(vec![node2.get_node_pk(), node3.get_node_pk()], &*node3.keys_manager, &secp_ctx).unwrap();
+	blinded_route.blinded_hops.clear();
+
+	let err = node1.messenger.send_onion_message(vec![], Destination::BlindedRoute(blinded_route)).unwrap_err();
+	assert_eq!(err, SendError::MissingBlindedHops);
+}
