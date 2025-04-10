9b08333fb555a94b4f35b4f3a947242633bdd12e
Valentine Wallace <vwallace@protonmail.com>
2025-04-10 15:26:12 -0400

    Track cached async receive offers in OffersMessageFlow
    In future commits, as part of being an async recipient, we will interactively
    build offers and static invoices with an always-online node that will serve
    static invoices on our behalf.
    
    Once an offer is built and we've requested persistence of the corresponding
    invoice from the server, we will use the new offer cache added here to save the
    invoice metadata and the offer in ChannelManager, though the OffersMessageFlow
    is responsible for keeping the cache updated.
    
    We want to cache and persist these offers so we always have them at the ready,
    we don't want to begin the process of interactively building an offer the
    moment it is needed. The offers are likely to be long-lived so caching them
    avoids having to keep interactively rebuilding them after every restart.

---
+++ lightning/src/offers/async_receive_offer_cache.rs
@@ -0,0 +1,147 @@
+// This file is Copyright its original authors, visible in version control
+// history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+
+//! Data structures and methods for caching offers that we interactively build with a static invoice
+//! server as an async recipient. The static invoice server will serve the resulting invoices to
+//! payers on our behalf when we're offline.
+
+use crate::blinded_path::message::BlindedMessagePath;
+use crate::io;
+use crate::io::Read;
+use crate::ln::msgs::DecodeError;
+use crate::offers::nonce::Nonce;
+use crate::offers::offer::Offer;
+use crate::onion_message::messenger::Responder;
+use crate::prelude::*;
+use crate::util::ser::{Readable, Writeable, Writer};
+use core::time::Duration;
+
+/// The status of this offer in the cache.
+enum OfferStatus {
+	/// This offer has been returned to the user from the cache, so it needs to be stored until it
+	/// expires and its invoice needs to be kept updated.
+	Used,
+	/// This offer has not yet been returned to the user, and is safe to replace to ensure we always
+	/// have a maximally fresh offer. We always want to have at least 1 offer in this state,
+	/// preferably a few so we can respond to user requests for new offers without returning the same
+	/// one multiple times. Returning a new offer each time is better for privacy.
+	Ready {
+		/// If this offer's invoice has been persisted for some time, it's safe to replace to ensure we
+		/// always have the freshest possible offer available when the user goes to pull an offer from
+		/// the cache.
+		invoice_confirmed_persisted_at: Duration,
+	},
+	// This offer's invoice is not yet confirmed as persisted by the static invoice server.
+	Pending,
+}
+
+struct AsyncReceiveOffer {
+	offer: Offer,
+	/// Whether this offer is used, ready for use, or pending invoice persistence with the static
+	/// invoice server.
+	status: OfferStatus,
+
+	/// The below fields are used to generate and persist a new static invoice with the invoice
+	/// server. We support automatically rotating the invoice for long-lived offers so users don't
+	/// have to update the offer they've posted on e.g. their website if fees change or the invoices'
+	/// payment paths become otherwise outdated.
+	offer_nonce: Nonce,
+	update_static_invoice_path: Responder,
+}
+
+impl_writeable_tlv_based_enum!(OfferStatus,
+	(0, Used) => {},
+	(1, Ready) => {
+		(0, invoice_confirmed_persisted_at, required),
+	},
+	(2, Pending) => {},
+);
+
+impl_writeable_tlv_based!(AsyncReceiveOffer, {
+	(0, offer, required),
+	(2, offer_nonce, required),
+	(4, status, required),
+	(6, update_static_invoice_path, required),
+});
+
+/// If we are an often-offline recipient, we'll want to interactively build offers and static
+/// invoices with an always-online node that will serve those static invoices to payers on our
+/// behalf when we are offline.
+///
+/// This struct is used to cache those interactively built offers, and should be passed into
+/// [`OffersMessageFlow`] on startup as well as persisted whenever an offer or invoice is updated.
+///
+/// [`OffersMessageFlow`]: crate::offers::flow::OffersMessageFlow
+pub struct AsyncReceiveOfferCache {
+	offers: Vec<Option<AsyncReceiveOffer>>,
+	/// Used to limit the number of times we request paths for our offer from the static invoice
+	/// server.
+	#[allow(unused)] // TODO: remove when we get rid of async payments cfg flag
+	offer_paths_request_attempts: u8,
+	/// Used to determine whether enough time has passed since our last request for offer paths that
+	/// more requests should be allowed to go out.
+	#[allow(unused)] // TODO: remove when we get rid of async payments cfg flag
+	last_offer_paths_request_timestamp: Duration,
+	///
+	#[allow(unused)] // TODO: remove when we get rid of async payments cfg flag
+	paths_to_static_invoice_server: Vec<BlindedMessagePath>,
+	///
+	#[allow(unused)] // TODO: remove when we get rid of async payments cfg flag
+	max_invoices_stored_by_server: u8,
+}
+
+impl AsyncReceiveOfferCache {
+	/// Creates an empty [`AsyncReceiveOfferCache`] to be passed into [`OffersMessageFlow`].
+	///
+	/// [`OffersMessageFlow`]: crate::offers::flow::OffersMessageFlow
+	pub fn new() -> Self {
+		Self {
+			offers: Vec::new(),
+			offer_paths_request_attempts: 0,
+			last_offer_paths_request_timestamp: Duration::from_secs(0),
+			paths_to_static_invoice_server: Vec::new(),
+			max_invoices_stored_by_server: 0,
+		}
+	}
+
+	///
+	pub(super) fn paths_to_static_invoice_server(&self) -> Vec<BlindedMessagePath> {
+		self.paths_to_static_invoice_server.clone()
+	}
+}
+
+impl Writeable for AsyncReceiveOfferCache {
+	fn write<W: Writer>(&self, w: &mut W) -> Result<(), io::Error> {
+		write_tlv_fields!(w, {
+			(0, self.offers, required_vec),
+			(2, self.paths_to_static_invoice_server, required_vec),
+			(4, self.max_invoices_stored_by_server, required),
+			// offer paths request retry info always resets on restart
+		});
+		Ok(())
+	}
+}
+
+impl Readable for AsyncReceiveOfferCache {
+	fn read<R: Read>(r: &mut R) -> Result<Self, DecodeError> {
+		_init_and_read_len_prefixed_tlv_fields!(r, {
+			(0, offers, required_vec),
+			(2, paths_to_static_invoice_server, required_vec),
+			(4, max_invoices_stored_by_server, required),
+		});
+		let offers: Vec<Option<AsyncReceiveOffer>> = offers;
+		Ok(Self {
+			offers,
+			offer_paths_request_attempts: 0,
+			last_offer_paths_request_timestamp: Duration::from_secs(0),
+			paths_to_static_invoice_server,
+			max_invoices_stored_by_server: max_invoices_stored_by_server.0.unwrap(),
+		})
+	}
+}
